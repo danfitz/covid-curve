df1581d408ea78bf0adb89aded8cc88e1cb704fb
Dan Fitz <dan@danfitz.com>
2020-05-05 18:33:36 -0400

    Converted GrowthForecast to use median instead of mean (performing the calculations in a util function)

--- src/components/GrowthForecast.js
+++ src/components/GrowthForecast.js
@@ -14,13 +14,15 @@ import Modal from './UI/Modal'
 import { Pgh } from './UI/textComponents'
 import Hide from './UI/Hide'
 import theme from '../theme'
+// Utils
+import { calcMedianGrowthFactor } from '../utils'
 
 const Table = styled.table`
   border-collapse: collapse;
   margin: 0 auto;
 
   th, td {
-    border: 0.05rem solid ${({ theme }) => theme.colors.text };
+    border: 0.05rem solid ${({ theme }) => theme.colors.text};
     padding: 0.5rem;
   }
 `
@@ -42,20 +44,20 @@ export const GrowthForecast = ({
   const filteredCases = cases.filter(c => c.growthFactor !== undefined) // ignores empty values
   const recentCases = filteredCases.slice(filteredCases.length - 14) // last 14 reported days
 
-  const avgGrowthFactor = recentCases.reduce((a, b) => a + b.growthFactor, 0) / recentCases.length // mean
+  const medianGrowthFactor = calcMedianGrowthFactor(recentCases)
 
   let healthCheck
   let healthColor
-  if (avgGrowthFactor >= 1.15) {
+  if (medianGrowthFactor >= 1.15) {
     healthCheck = 'Not flattening'
     healthColor = 'red'
-  } else if (avgGrowthFactor > 1) {
+  } else if (medianGrowthFactor > 1) {
     healthCheck = 'Not flattening but growing more slowly'
     healthColor = 'orange'
-  } else if (avgGrowthFactor > 0.85) {
+  } else if (medianGrowthFactor > 0.85) {
     healthCheck = 'Starting to flatten'
     healthColor = 'darkgreen'
-  } else if (avgGrowthFactor > 0) {
+  } else if (medianGrowthFactor > 0) {
     healthCheck = 'Flattening quickly'
     healthColor = 'green'
   } else {
@@ -64,7 +66,7 @@ export const GrowthForecast = ({
   }
 
   const [modalOpen, setModalOpen] = useState(false)
-  
+
   return (
     <React.Fragment>
       <Flex
@@ -82,7 +84,7 @@ export const GrowthForecast = ({
           alignItems: 'center'
         }}
       >
-        { recentCases.length ? (
+        {recentCases.length ? (
           <>
             <Heading
               as='h2'
@@ -115,7 +117,7 @@ export const GrowthForecast = ({
                   style={{ fontWeight: 400, color: 'black' }}
                   key={unit}
                   value={unit}
-                >  
+                >
                   {unit === 'All' ? 'Ontario' : unit}
                 </option>
               ))}
@@ -146,14 +148,14 @@ export const GrowthForecast = ({
             </Button>
           </>
         ) : (
-          <Flex
-            justifyContent='center'
-            alignItems='center'
-            height='100%'
-          >
-            <BeatLoader data-test='loader' color={theme.colors.primary} loading={true} size={20} />
-          </Flex>
-        ) }
+            <Flex
+              justifyContent='center'
+              alignItems='center'
+              height='100%'
+            >
+              <BeatLoader data-test='loader' color={theme.colors.primary} loading={true} size={20} />
+            </Flex>
+          )}
       </Flex>
 
       <Modal
@@ -166,28 +168,28 @@ export const GrowthForecast = ({
           <span style={{ color: theme.colors.primary }}>
             {healthUnit === 'All' ? 'Ontario' : healthUnit}
           </span>
-          , average growth factor has been
+          , median growth factor has been
           {' '}
           <span style={{ color: healthColor }}>
-            {avgGrowthFactor.toFixed(2)}
+            {medianGrowthFactor}
           </span>
         </Heading>
-        
+
         <Pgh color='unfocusedText' textAlign='center' mb={2}>
           <span style={{ fontWeight: 700 }}>Rule of thumb: </span>
           We know the curve is flattening when the growth factor is less than or equal to 1
         </Pgh>
 
         <ChartCard
-            data-test='growthChart'
-            data={recentCases}
-            title={`Growth factor (last ${recentCases.length} reported days)`}
-            dataKey='growthFactor'
-            displayType='average'
-            height='15rem'
-            icon='chart-line'
-            color={theme.colors.tertiary} />
-            
+          data-test='growthChart'
+          data={recentCases}
+          title={`Growth factor (last ${recentCases.length} reported days)`}
+          dataKey='growthFactor'
+          displayType='average'
+          height='15rem'
+          icon='chart-line'
+          color={theme.colors.tertiary} />
+
         <Heading as='h3' mt={5} mb={2}>Legend</Heading>
         <Table>
           <thead>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,22 +1,42 @@
 export const includeGrowthFactors = (data, dataKey) => {
   const newData = []
-  
+
   for (let i = 0; i < data.length; i++) {
     const newDatum = { ...data[i] }
 
     // Start at 3rd item
     if (i >= 2) {
-      const todaysChange = data[i][dataKey] - data[i-1][dataKey]
-      const yesterdaysChange = data[i-1][dataKey] - data[i-2][dataKey]
+      const todaysChange = data[i][dataKey] - data[i - 1][dataKey]
+      const yesterdaysChange = data[i - 1][dataKey] - data[i - 2][dataKey]
       const growthFactor = todaysChange / yesterdaysChange
-  
+
       if (isFinite(growthFactor)) {
         newDatum.growthFactor = parseFloat(growthFactor.toFixed(2))
       }
     }
-    
+
     newData.push(newDatum)
   }
 
   return newData
+}
+
+export const calcMedianGrowthFactor = cases => {
+  if (cases.length === 0) return undefined
+
+  const sortedCases = cases.sort((a, b) => a.growthFactor - b.growthFactor)
+  console.log(sortedCases)
+
+  let medianGrowthFactor
+
+  if (sortedCases.length % 2 !== 0) {
+    const middleCase = sortedCases[Math.floor(sortedCases.length / 2)]
+    medianGrowthFactor = middleCase.growthFactor
+  } else {
+    const middleLeftCase = sortedCases[(sortedCases.length / 2) - 1]
+    const middleRightCase = sortedCases[sortedCases.length / 2]
+    medianGrowthFactor = (middleLeftCase.growthFactor + middleRightCase.growthFactor) / 2
+  }
+
+  return medianGrowthFactor.toFixed(2)
 }
